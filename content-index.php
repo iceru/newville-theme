bb630683a62220743c00973e8ed6dd2e206d9566
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-10-15 01:05:26 +0700

    first commit

---
+++ content-index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template for displaying content in the index.php template.
+ */
+?>
+
+
+<div class="body">
+	<div class="header">
+		NEW LIFE OF THE NEXT GENERATION
+	</div>
+</div>
