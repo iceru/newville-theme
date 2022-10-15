b977a99cafd2495bf49d3c93a35bf97fa6367343
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-10-15 10:12:05 +0700

    update header and subheader

--- content-index.php
+++ content-index.php
@@ -5,26 +5,71 @@
 ?>
 
 
-<div class="body">
-	<div class="container">
-		<div class="header" id="top">
-			<h2>NEW LIFE OF THE NEXT GENERATION</h2>
-			<h6>次世代の新しい命 | 다음 세대의 새로운 삶 | 下一代的新生活</h6>
+<div class="body p-0">
+	<div class="header" id="top">
+		<div class="background">
+			<img src="<?php bloginfo('template_directory');?> /images/bg-header.png" alt="Newville">
 		</div>
-		<div class="sub-header">
-			<div class="subtitle">
-				A PEACEFUL ENCLAVE FOR
+		<div class="background2">
+			<img src="<?php bloginfo('template_directory');?> /images/header-5.png" alt="Newville">
+		</div>
+		<div class="container-fluid p-0">
+			<div class="top-header">
+				<div class="cloth">
+					<img class="w-100" src="<?php bloginfo('template_directory');?> /images/cloth.png" alt="Newville">
+				</div>
+				<div class="logo1">
+					<img src="<?php bloginfo('template_directory');?> /images/header-2.png" alt="Newville">
+				</div>
+				<div class="logo2">
+					<img src="<?php bloginfo('template_directory');?> /images/header-3.png" alt="Newville">
+				</div>
+				<div class="text">
+					<h2 class="title">NEW LIFE OF THE NEXT GENERATION</h2>
+					<h6 class="subtitle">次世代の新しい命 | 다음 세대의 새로운 삶 | 下一代的新生活</h6>
+				</div>
 			</div>
-			<div class="title">
-				LIVE, WORK, AND PLAY
-			</div>
-			<div class="text">
-				Kami belajar dari tempat-tempat perkotaan di Eropa, Jepang, Korea, China, dan banyak lagi tempat tinggal
-				perkotaan yang serupa untuk menciptakan gaya hidup perkotaan yang dinamis bagi generasi berikutnya untuk
-				menjalani kehidupan yang lebih baik di Newville.
+			<div class="images">
+				<div class="left-img">
+					<img src="<?php bloginfo('template_directory');?> /images/header-4.png" alt="Newville">
+				</div>
+				<div class="right-img">
+					<img src="<?php bloginfo('template_directory');?> /images/alpha.png" alt="Newville">
+				</div>
 			</div>
 		</div>
-		<div class="locations" id="locations">
+
+	</div>
+	<div class="sub-header">
+		<div class="background">
+			<img src="<?php bloginfo('template_directory');?> /images/bg-subheader.png" alt="Subheader">
+		</div>
+		<div class="background2">
+			<img src="<?php bloginfo('template_directory');?> /images/bg-subheader-2.png" alt="Subheader">
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="subtitle">
+						A PEACEFUL ENCLAVE FOR
+					</div>
+					<div class="title">
+						LIVE, WORK, AND PLAY
+					</div>
+					<div class="text">
+						Kami belajar dari tempat-tempat perkotaan di Eropa, Jepang, Korea, China, dan banyak lagi tempat
+						tinggal
+						perkotaan yang serupa untuk menciptakan gaya hidup perkotaan yang dinamis bagi generasi
+						berikutnya
+						untuk
+						menjalani kehidupan yang lebih baik di Newville.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="locations" id="locations">
+		<div class="container">
 			<div class="row">
 				<div class="col-12 col-lg-4">
 					<div class="title">
@@ -56,10 +101,187 @@
 				</div>
 			</div>
 		</div>
-
 	</div>
+
 	<div class="surroundings" id="surroundings">
 		<img class="w-100" src="<?php bloginfo('template_directory');?> /images/surroundings.png"
 			alt="Newville Surroundings">
+	</div>
+	<div class="features" id="features">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-lg-6">
+					<div class="people">
+						<img src="<?php bloginfo('template_directory');?> /images/entertainment-man.png"
+							alt="Entertainment">
+					</div>
+					<div class="logo1">
+						<img src="<?php bloginfo('template_directory');?> /images/entertainment-logo1.png"
+							alt="Entertainment">
+					</div>
+					<div class="logo2">
+						<img src="<?php bloginfo('template_directory');?> /images/entertainment-logo2.png"
+							alt="Entertainment">
+					</div>
+				</div>
+				<div class="col-12 col-lg-6">
+					<div class="title">
+						ENTERTAINMENT
+					</div>
+					<div class="subtitle">
+						HUB
+					</div>
+					<div class="text">
+						Untuk memberikan keseimbangan hidup, New Lippo Cikarang juga menciptakan pusat yang menyenangkan
+						dan berwarna untuk bersantai, bersenang-senang, berkuliner, dan banyak lagi pengalaman yang
+						tiada duanya.
+					</div>
+				</div>
+			</div>
+			<div class="row right-side">
+				<div class="col-12 col-lg-6">
+					<div class="title">
+						BUSINESS & COMMERCE
+					</div>
+					<div class="subtitle">
+						HUB
+					</div>
+					<div class="text">
+						Sebagai salah satu lokasi paling strategis di koridor timur, New Lippo Cikarang menciptakan
+						pusat bisnis dan perdagangan baru untuk memenuhi kebutuhan bisnis di kawasan tersebut.
+					</div>
+				</div>
+				<div class="col-12 col-lg-6">
+					<div class="people">
+						<img src="<?php bloginfo('template_directory');?> /images/entertainment-man.png"
+							alt="Entertainment">
+					</div>
+					<div class="logo1">
+						<img src="<?php bloginfo('template_directory');?> /images/entertainment-logo1.png"
+							alt="Entertainment">
+					</div>
+					<div class="logo2">
+						<img src="<?php bloginfo('template_directory');?> /images/entertainment-logo2.png"
+							alt="Entertainment">
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 col-lg-6">
+					<div class="people">
+						<img src="<?php bloginfo('template_directory');?> /images/entertainment-man.png"
+							alt="Entertainment">
+					</div>
+					<div class="logo1">
+						<img src="<?php bloginfo('template_directory');?> /images/entertainment-logo1.png"
+							alt="Entertainment">
+					</div>
+					<div class="logo2">
+						<img src="<?php bloginfo('template_directory');?> /images/entertainment-logo2.png"
+							alt="Entertainment">
+					</div>
+				</div>
+				<div class="col-12 col-lg-6">
+					<div class="title">
+						WELlNESS
+					</div>
+					<div class="subtitle">
+						HUB
+					</div>
+					<div class="text">
+						Terintegrasi dengan banyak lingkungan hijau, Wellness Hub memberikan tempat terbaik untuk hidup
+						dan meningkatkan kesehatan dalam hidup.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="view">
+		<img class="w-100" src="<?php bloginfo('template_directory');?> /images/view.png" alt="Newville">
+	</div>
+	<div class="units" id="units">
+		<div class="container">
+			<div class="row superior unit">
+				<div class="col-12 col-lg-4">
+					<div class="subtitle">
+						Type
+					</div>
+					<div class="title">
+						Superior
+					</div>
+					<button type="button" class="btn btn-outline-light">View More</button>
+				</div>
+				<div class="col-12 col-lg-8">
+					<img class="w-100" src="<?php bloginfo('template_directory');?> /images/superior.png"
+						alt="Superior">
+				</div>
+			</div>
+			<div class="row deluxe unit">
+				<div class="col-12 col-lg-8">
+					<img class="w-100" src="<?php bloginfo('template_directory');?> /images/Deluxe.png" alt="Deluxe">
+				</div>
+				<div class="col-12 col-lg-4">
+					<div class="subtitle">
+						Type
+					</div>
+					<div class="title">
+						Deluxe
+					</div>
+					<button type="button" class="btn btn-outline-light">View More</button>
+				</div>
+			</div>
+			<div class="row suite unit">
+				<div class="col-12 col-lg-4">
+					<div class="subtitle">
+						Type
+					</div>
+					<div class="title">
+						Suite
+					</div>
+					<button type="button" class="btn btn-outline-light">View More</button>
+				</div>
+				<div class="col-12 col-lg-8">
+					<img class="w-100" src="<?php bloginfo('template_directory');?> /images/Suite.png" alt="Suite">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="tour" id="360Tour">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-lg-6">
+					<div class="subtitle">
+						EXPLORE MORE IN OUR
+					</div>
+					<div class="title">
+						360 TOUR
+					</div>
+					<button type="button" class="btn btn-outline-light">View</button>
+				</div>
+				<div class="col-12 col-lg-6 images">
+					<div class="logo">
+						<img class="w-100" src="<?php bloginfo('template_directory');?> /images/view.png"
+							alt="Newville">
+					</div>
+					<div class="building">
+						<img class="w-75" src="<?php bloginfo('template_directory');?> /images/alpha.png"
+							alt="Newville">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="contact">
+		<div class="container">
+			<div class="row">
+				<div class="subtitle">
+					Begin Your
+				</div>
+				<div class="title">
+					Global <br>
+					Lifestyle
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
